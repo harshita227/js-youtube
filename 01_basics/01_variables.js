c1aa43aeae8bd235c057bd8310562259c11f477b
harshita227 <77845520+harshita227@users.noreply.github.com>
2023-12-01 10:27:00 +0000

    commit done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=1223
+let accoutEmail="hiteshagg@gma"
+var accountPass="12345"
+accuntCity="jaipur"
+let accountState;
+accountEmail="harshita@gmail.com"
+ accountPass="11111"
+ accountCity="Bengluru"
+ console.log(accountId);
+ console.table([accountId,accountPass,accountState,accoutEmail,accountCity])
+ 
---
+++ 02_datatypes.js
@@ -0,0 +1,11 @@
+let name="HArshita"
+let age=98
+let gem=null
+let state;
+//number 2 power 55
+//bigInt to store bigg number
+//string=""
+//boolean==true/false
+//null=standalone value iska type hai object
+//undefined =undefined hi type hai mtlb value defined nhi hai
+console.log(typeof "Hylr")
